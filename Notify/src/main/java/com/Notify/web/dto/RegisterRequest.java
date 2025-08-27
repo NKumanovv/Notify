37e54ce922b5233f3414bb898ae24057d34c063c
NikolaiK420 <nikolaikumanov006@gmail.com>
2025-08-27 17:55:55 +0300

    Update security config and dependencies, add WebMvcConfiguration
    Replaces BCryptPasswordEncoder with NoOpPasswordEncoder in BeanConfiguration for password encoding. Adds a new WebMvcConfiguration class to define security filter chain and HTTP security rules. Updates Lombok dependency version and scope in pom.xml. Removes unused validation imports from RegisterRequest.

--- Notify/src/main/java/com/Notify/web/dto/RegisterRequest.java
+++ Notify/src/main/java/com/Notify/web/dto/RegisterRequest.java
@@ -1,7 +1,6 @@
 package com.notify.web.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
