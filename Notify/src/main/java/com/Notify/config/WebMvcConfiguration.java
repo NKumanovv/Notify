efd74612cfe905686021a58149684c1d10391935
NikolaiK420 <nikolaikumanov006@gmail.com>
2025-08-27 19:35:54 +0300

    Refactor logout configuration in SecurityFilterChain
    Replaces AntPathRequestMatcher with logoutUrl for logout configuration and adds @Bean annotation to securityFilterChain method. This simplifies the logout setup and ensures proper bean registration.

--- Notify/src/main/java/com/Notify/config/WebMvcConfiguration.java
+++ Notify/src/main/java/com/Notify/config/WebMvcConfiguration.java
@@ -1,17 +1,18 @@
 package com.notify.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableMethodSecurity
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
@@ -26,7 +27,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                         .failureUrl("/login?error")
                         .permitAll())
                 .logout(logout -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                        .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
                 );
 
